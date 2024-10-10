0583cb02b4bec39f992110bc8ec0ca3311f8b742
Сторож Евгений <storozhea@it2g.ru>
2024-10-10 14:06:47 +0300

    правки

--- bot.js
+++ bot.js
@@ -5,13 +5,21 @@ const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
 let shoppingList = [];
-let selectedProducts = []; // Глобальный массив для хранения выбранных продуктов
+let selectedProducts = [];
+
+const COMMANDS = {
+    TOGGLE: 'toggle',
+    DELETE_SELECTED: 'delete_selected',
+    SHOW_LIST: 'Показать список',
+    ADD_ITEMS: 'Добавить продукты',
+    DELETE_ITEMS: 'Удалить продукты',
+    CLEAR_LIST: 'Очистить список',
+}
 
 bot.onText(/\/start/, (msg) => {
     sendWelcomeMessage(msg.chat.id);
 });
 
-// Функция для отправки приветственного сообщения
 function sendWelcomeMessage(chatId) {
     const welcomeMessage = `
     Добро пожаловать! Я помогу вам с покупками.
@@ -23,12 +31,12 @@ function sendWelcomeMessage(chatId) {
         reply_markup: {
             keyboard: [
                 [
-                    { text: "Показать список" },
-                    { text: "Добавить продукты" }
+                    { text: COMMANDS.SHOW_LIST },
+                    { text: COMMANDS.ADD_ITEMS },
                 ],
                 [
-                    { text: "Удалить продукты" },
-                    { text: "Очистить список" }
+                    { text: COMMANDS.DELETE_ITEMS },
+                    { text: COMMANDS.CLEAR_LIST }
                 ],
             ],
             resize_keyboard: true,
@@ -39,83 +47,100 @@ function sendWelcomeMessage(chatId) {
     bot.sendMessage(chatId, welcomeMessage, options);
 }
 
-// Обработка текстовых сообщений
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
 
     if (msg.text === "Начать") {
-        sendWelcomeMessage(chatId);
-    } else if (msg.text === "Показать список") {
-        bot.sendMessage(chatId, `Ваш список покупок:\n${shoppingList.length === 0 ? 'Список покупок пуст.' : shoppingList.join('\n')}`);
-    } else if (msg.text === "Добавить продукты") {
-        bot.sendMessage(chatId, 'Введите список продуктов через Enter:').then(() => {
+        return sendWelcomeMessage(chatId);
+    } 
+
+    if (msg.text === COMMANDS.SHOW_LIST) {
+        return bot.sendMessage(chatId, `Ваш список покупок:\n${shoppingList.length ? shoppingList.join('\n') : 'Список покупок пуст.'}`);
+    }
+
+    if (msg.text === COMMANDS.ADD_ITEMS) {
+        return bot.sendMessage(chatId, 'Введите список продуктов через Enter:').then(() => {
             bot.once('message', (msg) => {
                 const products = msg.text.split('\n').map(item => item.trim()).filter(item => item !== '');
+
                 shoppingList.push(...products);
+
                 bot.sendMessage(chatId, `Добавлены продукты:\n${products.join('\n')}`);
             });
         });
-    } else if (msg.text === "Удалить продукты") {
-        showDeleteOptions(chatId);
-    } else if (msg.text === "Очистить список") {
+    } 
+
+    if (msg.text === COMMANDS.DELETE_ITEMS) {
+        return showDeleteOptions(chatId);
+    } 
+
+    if (msg.text === COMMANDS.CLEAR_LIST) {
         shoppingList = [];
-        bot.sendMessage(chatId, 'Список покупок очищен.');
+
+        return bot.sendMessage(chatId, 'Список покупок очищен.');
     }
 });
 
-// Функция для отображения продуктов для удаления
+function getOptions() {
+    return {
+        reply_markup: {
+            inline_keyboard: shoppingList.map((item, index) => [
+                { text: `${selectedProducts.includes(index) ? '✔️' : ''} ${item}`, callback_data: `${COMMANDS.TOGGLE}_${index}` }
+            ]).concat([[{ text: "Удалить выбранные", callback_data: COMMANDS.DELETE_SELECTED }]])
+        }
+    };
+}
+
 function showDeleteOptions(chatId) {
-    if (shoppingList.length === 0) {
+    if (!shoppingList.length) {
         bot.sendMessage(chatId, 'Список покупок пуст.');
+
         return;
     }
 
-    const options = {
-        reply_markup: {
-            inline_keyboard: shoppingList.map((item, index) => [
-                { text: `${selectedProducts.includes(index) ? '✔️' : ''} ${item}`, callback_data: `toggle_${index}` }
-            ]).concat([[{ text: "Удалить выбранные", callback_data: "delete_selected" }]])
-        }
-    };
+    const options = getOptions();
+
 
     bot.sendMessage(chatId, 'Выберите продукты для удаления:', options).then(sentMessage => {
-        // Сохраняем ID сообщения для редактирования
         sentMessage.message_id = sentMessage.message_id;
     });
 }
 
-// Обработка нажатий на кнопки удаления
 bot.on('callback_query', (callbackQuery) => {
     const chatId = callbackQuery.message.chat.id;
     const action = callbackQuery.data;
 
-    if (action.startsWith('toggle_')) {
+    if (action.startsWith(COMMANDS.TOGGLE)) {
         const index = parseInt(action.split('_')[1]);
+
         if (selectedProducts.includes(index)) {
-            selectedProducts = selectedProducts.filter(i => i !== index); // Удаляем из выбранных
+            selectedProducts = selectedProducts.filter(indexSelected => indexSelected !== index);
         } else {
-            selectedProducts.push(index); // Добавляем в выбранные
+            selectedProducts.push(index);
         }
 
-        // Обновляем разметку кнопок в том же сообщении
-        const options = {
-            reply_markup: {
-                inline_keyboard: shoppingList.map((item, idx) => [
-                    { text: `${selectedProducts.includes(idx) ? '✔️' : ''} ${item}`, callback_data: `toggle_${idx}` }
-                ]).concat([[{ text: "Удалить выбранные", callback_data: "delete_selected" }]])
-            }
-        };
+        const options = getOptions()
 
-        bot.editMessageReplyMarkup(options.reply_markup, { chat_id: chatId, message_id: callbackQuery.message.message_id });
-    } else if (action === 'delete_selected') {
-        if (selectedProducts.length > 0) {
-            const removedProducts = selectedProducts.map(index => shoppingList[index]);
-            shoppingList = shoppingList.filter((_, index) => !selectedProducts.includes(index)); // Удаляем выбранные продукты
-            bot.sendMessage(chatId, `Удалены продукты:\n${removedProducts.join('\n')}`);
-            selectedProducts = []; // Очищаем выбранные
-        } else {
-            bot.sendMessage(chatId, 'Нет выбранных продуктов для удаления.');
-        }
-        // showDeleteOptions(chatId); // Показываем обновленный список
+        return bot.editMessageReplyMarkup(options.reply_markup, { chat_id: chatId, message_id: callbackQuery.message.message_id });
     }
+    
+    if (action === COMMANDS.DELETE_SELECTED && !selectedProducts.length) {
+        return bot.sendMessage(chatId, 'Нет выбранных продуктов для удаления.');
+    }
+
+    const removedProducts = [];
+
+    shoppingList = shoppingList.filter((item, index) => {
+        if(selectedProducts.includes(index)) {
+            removedProducts.push(item)
+
+            return false
+        }
+
+        return true
+    });
+
+    bot.sendMessage(chatId, `Удалены продукты:\n${removedProducts.join('\n')}`);
+
+    selectedProducts = [];
 });
